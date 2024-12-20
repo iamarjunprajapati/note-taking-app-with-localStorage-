5a9e58d778afbe1508ecaef16d95cb308a531e70
arjun2002211 <arjunprajapat2002211@gmail.com>
2024-12-20 17:36:25 +0530

    project complete.

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
-let textareas = [];
 let addButton = document.querySelector('#add-note');
 let textAreaWrapper = document.querySelector('.textarea-wrapper');
+
 function createNote() {
     let newTextArea = document.createElement('textarea');
     newTextArea.classList.add('note', 'md:w-[30%]', 'w-full', 'bg-[rgba(255,255,255,0.23)]', 'h-[200px]', 'shadow', 'hover:shadow-xl', 'transition', 'rounded-lg', 'px-3', 'text-sm', 'resize-none', 'focus:outline-none', 'text-white', 'py-4', 'mb-3');
     newTextArea.placeholder = "Enter your note...";
-    newTextArea.setAttribute('id', `${Date.now()}`);
-    textareas.push(newTextArea);
+    newTextArea.setAttribute('id', `${Date.now}`);
     textAreaWrapper.insertBefore(newTextArea, addButton);
-
-    textareas.forEach((textarea) => {
-        textarea.addEventListener('input', () => {
-            let noteData = {
-                id: textarea.id,
-                value: textarea.value,
-            }
-            localStorage.setItem(`textarea${textarea.id}`, JSON.stringify(noteData));
-        });
+    let allTextAreas = document.querySelectorAll('textarea');
+    allTextAreas.forEach((textarea) => {
+        textarea.addEventListener('input', (e) => {
+            textarea.value = e.target.value;
+            saveInput(textarea);
+        })
     })
 }
 
+function saveInput(textarea) {
+    let allTextAreas = document.querySelectorAll('textarea');
+    let data = Array.from(allTextAreas).map((textarea) => {
+        return {
+            value: textarea.value
+        }
+    });
 
-window.onload = () => {
-    console.log('working');
-    Object.keys(localStorage).forEach((textarea) => {
-        let values = localStorage.getItem(textarea);
-        let parsedValue = JSON.parse(values);
-        let newTextArea = document.createElement('textarea');
-        newTextArea.setAttribute('id', parsedValue.id);
+    localStorage.setItem("textareaData", JSON.stringify(data));
+}
+
+window.onload = function () {
+    let savedTextArea = JSON.parse(localStorage.getItem('textareaData')) || [];
+    let arrayTextArea = Array.from(savedTextArea);
+    console.log(arrayTextArea);
+    arrayTextArea.forEach((textarea, index) => {
+        let newTextArea = document.createElement('textarea')
         newTextArea.classList.add('note', 'md:w-[30%]', 'w-full', 'bg-[rgba(255,255,255,0.23)]', 'h-[200px]', 'shadow', 'hover:shadow-xl', 'transition', 'rounded-lg', 'px-3', 'text-sm', 'resize-none', 'focus:outline-none', 'text-white', 'py-4', 'mb-3');
-        newTextArea.value = parsedValue.value;
+        newTextArea.placeholder = "Enter your note...";
+        newTextArea.value = textarea.value
+        newTextArea.addEventListener('input', () => {
+            saveInput(newTextArea)
+        })
+        newTextArea.addEventListener('dblclick', () => {
+            deleteNote(index , newTextArea)
+        })
         textAreaWrapper.insertBefore(newTextArea, addButton);
 
     })
-};
+}
+
+function deleteNote(index, element) {
+    let savedTextArea = JSON.parse(localStorage.getItem('textareaData')) || [];
+    savedTextArea.splice(index, 1);
+    localStorage.setItem("textareaData", JSON.stringify(savedTextArea));
+    element.remove();
+    // window.onload();
+
+}
 
